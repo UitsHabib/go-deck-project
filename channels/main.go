71cabaea48228232e352fe1e7fffc1eaf1ab1667
bshabib <habibur.rahman@brainstation-23.com>
2022-05-18 18:55:58 +0600

    Go routines and protocol buffer adds.

--- channels/main.go
+++ channels/main.go
@@ -2,11 +2,48 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"sync"
 	"time"
 )
 
-func main() {
+func SendValue(c chan string) {
+	fmt.Println("Executing Goroutine")
+	time.Sleep(time.Second)
+	c <- "Hello world"
+	fmt.Println("Finished executing goroutine")
+}
+
+func call() {
+	fmt.Println("Go channels tutorial")
+
+	values := make(chan string, 2)
+	defer close(values)
+
+	go SendValue(values)
+	go SendValue(values)
+
+	value := <-values
+	fmt.Println(value)
+
+	time.Sleep(time.Second)
+}
+
+func checkLink(link string, c chan string) {
+	_, err := http.Get(link)
+
+	if err != nil {
+		fmt.Println(link, "might be down!")
+		c <- link
+		return
+	}
+
+	fmt.Println(link, "is up!")
+	c <- link
+}
+
+func call1() {
 	links := []string{
 		"http://google.com",
 		"http://facebook.com",
@@ -29,15 +66,55 @@ func main() {
 	}
 }
 
-func checkLink(link string, c chan string) {
-	_, err := http.Get(link)
+func myFunc(wg *sync.WaitGroup) {
+	time.Sleep(time.Second)
+	fmt.Println("Finished executing goroutines")
+	wg.Done()
+}
 
-	if err != nil {
-		fmt.Println(link, "might be down!")
-		c <- link
-		return
+func call2() {
+	fmt.Println("Go waitgroup tutorial")
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go myFunc(&wg)
+	wg.Wait() // blocks until 0
+	fmt.Println("Finished executing my go program")
+}
+
+func fetchStatus(w http.ResponseWriter, r *http.Request) {
+	var urls = []string{
+		"https://google.com",
+		"https://tutorialedge.net",
+		"https://twitter.com",
 	}
 
-	fmt.Println(link, "is up!")
-	c <- link
+	var wg sync.WaitGroup
+
+	for _, url := range urls {
+		wg.Add(1)
+
+		go func(url string) {
+			resp, err := http.Get(url)
+			if err != nil {
+				fmt.Fprintf(w, "%+v\n", err)
+			}
+			fmt.Fprintf(w, "%+v\n", resp)
+			wg.Done()
+		}(url)
+	}
+
+	wg.Wait()
+}
+
+func call3() {
+	fmt.Println("Go waitgroup tutorial")
+	http.HandleFunc("/", fetchStatus)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func main() {
+	// call()
+	// call1()
+	// call2()
+	call3()
 }
--- go.mod
+++ go.mod
@@ -4,11 +4,13 @@ go 1.18
 
 require (
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/jinzhu/gorm v1.9.16 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/mattn/go-sqlite3 v1.14.13 // indirect
+	google.golang.org/protobuf v1.26.0 // indirect
 	gorm.io/driver/mysql v1.3.3 // indirect
 	gorm.io/driver/sqlite v1.3.2 // indirect
 	gorm.io/gorm v1.23.5 // indirect
